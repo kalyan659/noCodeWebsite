8f3e429cf1964f2691b56842faaee958883b9081
kalyan659 <kalyanbratmedhi@google.com>
2025-07-16 22:20:31 +0530

    'initial3'

--- static/output-site/script.js
+++ static/output-site/script.js
@@ -1,6 +1,5 @@
 
-        document.querySelector('form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            alert('Thank you for contacting us!');
-        });
-    
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Message Sent!');
+  });
